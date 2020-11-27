d196344943c8266b0f83404762b4174813cc7dff
WaldirDMS <75144213+WaldirDMS@users.noreply.github.com>
2020-11-27 19:03:39 -0300

    Add files via upload

---
+++ ListaPOOex9/ListaPOOex9/Comparador.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaPOOex9
+{
+    class Comparador
+    {
+        private int valor1;
+        private int valor2;
+        private int maior;
+
+        public void setValor1(int p)
+        {
+            valor1 = p;
+        }
+
+        public void setValor2(int p)
+        {
+            valor2 = p;
+        }
+
+        public int getMaior()
+        {
+            return maior;
+        }
+
+        public void CompararValores()
+        {
+            if(valor1==valor2)
+            {
+               Console.WriteLine("Os valoes são iguais");
+            }
+
+            else
+
+                if (valor1 > valor2)
+                {
+                    maior = valor1;
+                }
+
+                else
+                {
+                     maior = valor2;
+                }
+        }
+    }
+}
---
+++ ListaPOOex9/ListaPOOex9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ListaPOOex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comparador comp;
+
+            comp = new Comparador();
+
+            Console.Write("Informe o primeiro valor ");
+            comp.setValor1(int.Parse(Console.ReadLine()));
+
+            Console.Write("Informe o segundo valor ");
+            comp.setValor2(int.Parse(Console.ReadLine()));
+
+            comp.CompararValores();
+
+            Console.WriteLine("O maior valor é {0}", comp.getMaior());
+        }
+    }
+}
